2e2a0ec8f23c838446db2d820682dc529dfaea8b
DESKTOP-R8FK64B\YERIN <seasparkle@naver.com>
2021-02-05 20:42:07 +0900

    Revert "Revert "Revert "Revert "환경설정 완료""""
    This reverts commit 6da7de854f59cb60567f1ff6ef43fe7dfb2eb098.

---
+++ rose-core/src/main/java/com/study/core/RoseCoreApplication.java
@@ -0,0 +1,12 @@
+package com.study.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoseCoreApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(RoseCoreApplication.class, args);
+	}
+
+}
