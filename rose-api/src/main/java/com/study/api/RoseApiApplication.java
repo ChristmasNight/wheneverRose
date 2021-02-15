6f312579ab7c3ef15cf19fb1e8ed53546d894e98
DESKTOP-R8FK64B\YERIN <seasparkle@naver.com>
2021-02-15 16:05:56 +0900

    rose-api jspRepository 활성화

--- rose-api/src/main/java/com/study/api/RoseApiApplication.java
+++ rose-api/src/main/java/com/study/api/RoseApiApplication.java
@@ -3,9 +3,11 @@ package com.study.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EntityScan(basePackages = {"com.study.core"}) 
+@EnableJpaRepositories(basePackages = {"com.study.core"})
 public class RoseApiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(RoseApiApplication.class, args);
